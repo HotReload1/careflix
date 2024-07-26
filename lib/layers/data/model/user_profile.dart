7c3985351a0998cedcffa9226191f21d58c9d966
Hot Reload <154380317+HotReload1@users.noreply.github.com>
2024-07-26 20:05:15 +0300

    Delete lib/layers/data directory

--- lib/layers/data/model/user_profile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:careflix/layers/data/model/show.dart';
-
-class UserProfile {
-  List<String>? userListIds;
-  List<String>? userFavouriteIds;
-
-  UserProfile({this.userListIds, this.userFavouriteIds});
-}
